3542b0a957bf01e451469cd0d03a0886cb8a05cb
fynniels12 <fynniels12@gmail.com>
2024-03-21 19:42:28 +0100

    Packet transfer works

--- Task1-Encrypted-ICMP-covert-channel/client.py
+++ Task1-Encrypted-ICMP-covert-channel/client.py
@@ -1,18 +1,28 @@
 
 import socket
 
+ICMP_TYPE = 47
+
 def read_address():
-    r = input("Enter the IP address of the server: ")
-    print("The IP address of the server is: ", r)
-    return r
+    addr = input("Enter the IP address of the server: ") or "localhost"
+    print("The IP address of the server is: ", addr)
+    port = input("Enter the port number of the server: ") or "12345"
+    print("The port number of the server is: ", port)
+
+    return addr, port
 
 
 def main():
-    server_address = read_address()
+    #server_address, port = read_address()
+    print("hello world")
 
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    s.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
-    s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    #s.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+    #s.connect((server_address, int(port)))
+    #s.sendall(b"Hello, world")
+    #s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    packet = b'\x47\x00\x00\x00\x69\x69\x69\x69\x69\x69\x69\x69\x69'  # ICMP HEADER + DATA (FULL HEADER IS REQUIRED)
+    s.sendto(packet, ("localhost", 700))  # 700 is just a random port
 
 
 
--- Task1-Encrypted-ICMP-covert-channel/server.py
+++ Task1-Encrypted-ICMP-covert-channel/server.py
@@ -3,13 +3,14 @@ import socket
 import struct
 
 SERVER_ADRRESS = "localhost"
-SERVER_PORT = 12345
+SERVER_PORT = 700
 
 
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     s.bind((SERVER_ADRRESS, SERVER_PORT))
+    print("Server is listening on port: ", SERVER_ADRRESS, SERVER_PORT, "\n")
 
     while True:
         recPacket, addr = s.recvfrom(1024)

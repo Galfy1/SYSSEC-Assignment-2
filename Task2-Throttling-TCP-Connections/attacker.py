40747c588c197249beb4c87d765012aa5605c184
fynniels12 <fynniels12@gmail.com>
2024-03-26 22:23:33 +0100

    further progress on task2..

--- Task2-Throttling-TCP-Connections/attacker.py
+++ Task2-Throttling-TCP-Connections/attacker.py
@@ -1,12 +1,22 @@
 
+from scapy.sendrecv import *
+
+def tcp_throttling(source_addr, dest_addr, approach = "ACK"):
+
+    sniffed_package = sniff(count = 20)
+    sniffed_package[1].show()
+
+    if approach == "ACK":
+        pass
+    elif approach == "RST":
+        pass
+    else:
+        raise ValueError("Incorrect or invalid approach")
 
 
 def main():
-    pass
-
-
-
-
+    tcp_throttling(12312312,123123,"ACK")
+    
 
 if __name__ == "__main__":
     main()
